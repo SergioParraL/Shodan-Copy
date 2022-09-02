e2d5f5b3ef9c38d3a381228e1964b7e9a87fd507
Sergio Parra <macuser@Macs-MacBook-Air.local>
2022-09-02 13:34:03 -0600

    this is the first commit, I making the webs components

---
+++ src/components/components.js
@@ -0,0 +1,106 @@
+class Header extends HTMLElement{ //Delivered
+    constructor(){
+        super()
+        this.attachShadow({mode : 'open'})
+    }
+
+    getResourses = () =>{
+        return`
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        `
+    }
+
+    getStylesNavBar = () => {
+        return`
+            <style>
+                .navOptions{
+                    display: inline-flex;
+                    justify-content: center;
+                    align-items: center;
+                }
+                .buttonBarrSearch{
+                    list-style: none;
+                    width: 5.2rem;
+                    position: relative;
+                    right: 20px;
+                }
+                .btnSearchBarr{
+                    background-color: transparent;
+                    height: 4rem;
+                    border: none;
+                }
+                .buttonBarrSearch a i{
+                    line-height: 60px;
+                }
+                .buttonBarrSearch a{
+                    padding: 0;
+                    margin: 0;
+                    width: 100%;
+                    height: 4.4rem;
+                    position: relative;
+                    bottom: 3px;
+                }
+                .navBarr{
+                    margin-left: .7rem;
+                    position: relative;
+                    bottom: 105%;
+                    z-index: 10;
+                }
+                nav{
+                    background-color: #212121;
+                }
+                .nav-wrapper{
+                    background-color: #2F2F2F;
+                }
+                .navOptions:hover{
+                    background-color: black;
+                }
+            </style>
+        `
+    } 
+
+    getNavBarTemplate(){
+        const navBar = document.createElement('template')
+        navBar.innerHTML = `
+            <div class="row navBarr">
+                <nav id="nav" class="col s6">
+                    <div class="navOptions col s2"><a href='#'>Sergio</a></div>
+                    <div class="navOptions col s2"><a href='#'>Explore</a></div>
+                    <div class="navOptions col s2"><a href='#'>GitHub</a></div>
+                    <div class="col s6">
+                        <nav>
+                            <div class="nav-wrapper">
+                            <form>
+                                <div class="input-field">
+                                <input id="search" type="search" required>
+                                <label class="label-icon" for="search"><i class="material-icons"><ion-icon name="search-outline"></ion-icon></i></label>
+                                <i class="material-icons"><ion-icon name="close-outline"></ion-icon></i>
+
+                                </div>
+                            </form>
+                            </div>
+                        </nav>
+                    </div>
+                </nav>
+            </div>
+
+            ${this.getResourses()}
+            ${this.getStylesNavBar()}
+            `
+            // Si pasas la mano por el icono de busqueda cambia a una mano, y al darle click hace la busqueda
+        return navBar
+    }
+
+    render(){
+        this.shadowRoot.appendChild(this.getNavBarTemplate().content.cloneNode('true'))
+    }
+
+    connectedCallback() {
+        this.render()
+    }
+
+}
+
+customElements.define('header-template', Header)
